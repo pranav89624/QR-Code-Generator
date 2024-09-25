892b343421e422e31721345f0a85212c7d9c209d
Pranav Verma <142308925+pranav89624@users.noreply.github.com>
2024-09-25 20:33:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from "fs";
+
+inquirer
+
+  .prompt([
+    {
+      "message": "Type in your URL: ",
+      name: "URL"
+    }
+  ])
+
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr-image.png'));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  
+  .catch((error) => {
+    if (error.isTtyError) {
+      alert("An Error Occured , Please Try Again !!!")
+    }
+  });
